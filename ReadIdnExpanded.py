5a89003abb882790ab0deb52603c65414d5e8a92
Eljay Gemoto <eljay@vektrex.com>
2020-02-24 14:29:12 -0800

    Created TcpSocket.py, made comments consistent across all files

--- ReadIdnExpanded.py
+++ ReadIdnExpanded.py
@@ -3,55 +3,21 @@
 # Example Result: Vektrex, SpikeSafe Mini, Rev 2.0.3.18; Ch 1: DSP 2.0.9, CPLD C.2, Last Cal Date: 17 FEB 2020, SN: 12006, HwRev: E1, Model: MINI-PRF-10-10US\n
 # Note: Written for expansion to add future commands
 
-import socket
 import sys
+import TcpSocket
 
-# create a connection via socket
-def openSocket(ip_address, port_number):
-    global tcp_socket
-    try:
-        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket object
-        tcp_socket.settimeout(2)                                        # 2 second socket timeout
-        tcp_socket.connect((ip_address, port_number))                   # connect to socket
-    except Exception as e:
-        print('Error connecting to socket at {}: {}'.format(ip_address, e))
-        raise
-
-# close a connection via socket
-def closeSocket():
-    try:
-        tcp_socket.close()  # disconnect from socket
-    except Exception as e:
-        print('Error disconnecting from socket: {}'.format(e))
-        raise
-    
-
-# send a SCPI command via socket
-def sendScpiCommand(scpi_command):
-    try:
-        scpi_command_str = scpi_command + '\n'           # add \n termination to SCPI command
-        scpi_command_byte = scpi_command_str.encode()    # encode SCPI command to byte
-        tcp_socket.send(scpi_command_byte)               # send byte to socket
-    except Exception as e:
-        print('Error sending SCPI command to socket: {}'.format(e))
-        raise
-
-# read data via socket
-def readData():
-    try:
-        data_str = tcp_socket.recv(2048)
-        return data_str
-    except Exception as e:
-        print('Error reading data from socket: {}'.format(e))
-        raise
+# set these before starting application
+ip_address = '10.0.0.246'   # SpikeSafe IP address
+port_number = 8282          # SpikeSafe port number
 
 # start of main program
 try:
-    openSocket('10.0.0.240', 8282)  # connect to SpikeSafe
-    sendScpiCommand('*IDN?')        # request SpikeSafe information
-    data = readData()               # read SpikeSafe information
-    print(data)                     # print SpikeSafe response to terminal
-    closeSocket()                   # disconnect from SpikeSafe
-except Exception as e:
-    print('Program error: {}'.format(e))
-    sys.exit(1)                     # exit application on any error
+    tcp_socket = TcpSocket.TcpSocket()              # instantiate new TcpSocket to connect to SpikeSafe
+    tcp_socket.openSocket(ip_address, port_number)  # connect to SpikeSafe
+    tcp_socket.sendScpiCommand('*IDN?')             # request SpikeSafe information
+    data = tcp_socket.readData()                    # read SpikeSafe information
+    print(data)                                     # print SpikeSafe response to terminal
+    tcp_socket.closeSocket()                        # disconnect from SpikeSafe
+except Exception as err:
+    print('Program error: {}'.format(err))          # print any error to terminal
+    sys.exit(1)                                     # exit application on any error
--- ReadIdnSimple.py
+++ ReadIdnSimple.py
@@ -5,22 +5,26 @@
 
 import socket
 
-ip_address = '10.0.0.240'   # SpikeSafe IP address
+# set these before starting application
+ip_address = '10.0.0.246'   # SpikeSafe IP address
 port_number = 8282          # SpikeSafe port number
 
+# start of main program
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket object
-tcp_socket.settimeout(5)
+tcp_socket.settimeout(2)                                        # 2 second socket timeout
 
 try:
-    tcp_socket.connect((ip_address, port_number))                   # connect to SpikeSafe
-    arg_str = "*IDN?\n"                                             # define SpikeSafe SCPI command with line feed /n as an argument to send from socket
-    arg_byte = arg_str.encode()                                     # convert argument to type byte, which is the format required by the socket
-    tcp_socket.send(arg_byte)                                       # send SpikeSafe SCPI command
-    response = tcp_socket.recv(2048)                                # read SpikeSafe response
-    print(response)                                                 # print SpikeSafe response to terminal
+    tcp_socket.connect((ip_address, port_number))   # connect to SpikeSafe
+    arg_str = "*IDN?\n"                             # define SpikeSafe SCPI command with line feed \n as an argument to send from socket
+    arg_byte = arg_str.encode()                     # convert argument to type byte, which is the format required by the socket
+    tcp_socket.send(arg_byte)                       # send SpikeSafe SCPI command
+    response = tcp_socket.recv(2048)                # read SpikeSafe response
+    print(response)                                 # print SpikeSafe response to terminal
 except socket.timeout as err:
     print(err)
 except socket.error as err:
     print(err)
+except Exception as err:
+    print(err)
 finally:
-    tcp_socket.close()                                              # disconnect from SpikeSafe
+    tcp_socket.close()                              # disconnect from SpikeSafe
