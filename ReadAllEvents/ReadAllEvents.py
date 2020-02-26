8b434566883f5dcda37c875cc64dbc9bf3cee486
Eljay Gemoto <eljay@vektrex.com>
2020-02-26 14:45:09 -0800

    Missed ReadAllEvents on last commit (#1)

---
+++ ReadAllEvents/ReadAllEvents.py
@@ -0,0 +1,45 @@
+# Goal: Retrive Spikesafe events from queue by reading all events
+# SCPI Command: *SYST:ERR?
+# SpikeSafe events are parsed into EventData class
+# Example message 1: 102, External Paused Signal Stopped
+# Example message 2: 200, Max Compliance Voltage; Channel(s) 1,2,3
+
+
+from ReadAllEvents import EventData
+
+def ReadAllEvents(spikeSafeSocket):
+    try:
+        # event list to be returned to caller
+        event_data_list = []
+
+        # initialize flag to check if event queue is empty 
+        is_event_queue_empty = False                                                                                                                      
+
+        # run as long as there is an event in the SpikeSafe queue
+        while is_event_queue_empty == False:
+            # request SpikeSafe events and read data
+            spikeSafeSocket.sendScpiCommand('SYST:ERR?')                                        
+            event_response = spikeSafeSocket.readData()                                        
+
+            if event_response != '':
+                 # parse all valid event responses from SpikeSafe into event_data class                                                           
+                event_data = EventData.EventData().ParseEventData(event_response)
+
+                if event_data.code != 0:
+                    # events with code greater than 0 are valid, add these to event data list
+                    event_data_list.append(event_data)                                  
+                else:
+                    # Example message: 0, OK
+                    # When "0,OK is read the SpikeSafe event queqe is empty
+                    is_event_queue_empty = True                                                 
+            else:
+                # unexpected response detected from SpikeSafe, end checking
+                raise Exception('No event response from SpikeSafe: {}'.format(event_response))  
+
+        # return event list to caller        
+        return event_data_list                                                                  
+    except Exception as err:
+        # print any error to terminal and raise error to function caller
+        print("Error emptying event queue: {}".format(err))                                     
+        raise                                                                             
+
