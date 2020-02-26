68b221a7a991c5f152e28a08df3caa1af47a0f4e
Eljay Gemoto <eljay@vektrex.com>
2020-02-26 11:32:49 -0800

    Implemented parsing of bulk voltage (#2)

--- GetStatus/MemoryTableReadUtility.py
+++ GetStatus/MemoryTableReadUtility.py
@@ -1,19 +1,35 @@
+# Goal: Parse SpikeSafe get status into an accessible object
+# Example status 1: (DIF (NAME "Output Readings" (DATA (BULK 99.7) (CH1 10.123456 1.123000 1) (T1 20.7) (T2 0.0) (T3 0.0) (T4 0.0) )))
+# Example status 2: (DIF (NAME "Output Readings" (DATA (BULK 99.7) (CH1 10.123456 1.123000 1) (CH2 0.000000 0.000000 0) (T1 20.7) (T2 21.0) (T3 0.0) (T4 0.0) )))
+
 from GetStatus import ChannelData
 from GetStatus import MemoryTableRead
 from GetStatus import TemperatureData
 
-def ParseMemoryTableRead(memory_table_read_str):
-    memory_table_read = MemoryTableRead.MemoryTableRead()
-    memory_table_read.bulk_voltage = __parseBulkVoltage__(memory_table_read_str)
+def ParseMemoryTableRead(get_status_response):
+    memory_table_read = MemoryTableRead.MemoryTableRead()                       # get status object to return
+    memory_table_read.bulk_voltage = __parseBulkVoltage__(get_status_response)  # extract bulk voltage from SpikeSafe get status response
+
     return memory_table_read
 
-def __parseBulkVoltage__(memory_table_read_str):
-    # bulk_start_index = memory_table_read_str.find('BULK') + 4
-    # bulk_voltage_str = memory_table_read_str.IndexOf(')', bulk_start_index)
-    bulk_voltage = float(0)
-    return bulk_voltage
+# e.g. parse "99.7" from (DIF (NAME "Output Readings" (DATA (BULK 99.7) (CH1 10.123456 1.123000 1) (T1 20.7) (T2 0.0) (T3 0.0) (T4 0.0) )))
+def __parseBulkVoltage__(get_status_response):
+    try:
+        bulk_voltage = None                                                                             # bulk voltage to return to caller
+        bulk_start_index = get_status_response.find(b"BULK ")                                           # find start of BULK in get status response
 
-"""     int PosBulk = reading.IndexOf("BULK") + 4;
-                int ParenEnd = reading.IndexOf(")", PosBulk);
-                string BulkVStr = reading.Substring(PosBulk + 1, ParenEnd - PosBulk - 1);
-                BulkVoltage = decimal.Parse(BulkVStr, NumberStyles.Number, CultureInfo.InvariantCulture); """
+        if bulk_start_index > -1: # ensure BULK was found
+            bulk_parenthesis_end_index = get_status_response.find(b")", bulk_start_index)               # find first ) after BULK
+            bulk_voltage_str = get_status_response[bulk_start_index + 5 : bulk_parenthesis_end_index]   # extract bulk voltage string
+
+            if isinstance(float(bulk_voltage_str), float) == True:                                      # ensure bulk voltage string is a float
+                bulk_voltage = float(bulk_voltage_str)                                                  # set bulk voltage as a float                                                   
+
+        if bulk_voltage == None:                                                                        # unexpected bulk voltage detected from SpikeSafe, end parsing
+            raise Exception('Could not parse bulk voltage from: {}'.format(get_status_response))
+
+        return bulk_voltage                                                                             # return bulk voltage to caller
+
+    except Exception as err:
+        print("Error parsing event code: {}".format(err))                                               # print any error to terminal
+        raise                                                                                           # raise error to function caller
