2dc728a44652a442909c8bb9aaee22afa9050e04
Eljay Gemoto <eljay@vektrex.com>
2020-03-05 15:05:17 -0800

    Add docStrings to TcpSocket class and openSocket function (#5)

--- Utility/SpikeSafeUtility/TcpSocket.py
+++ Utility/SpikeSafeUtility/TcpSocket.py
@@ -3,9 +3,40 @@
 import socket
 
 class TcpSocket():
+    """
+    A class used to represent a TCP socket for remote communication
+    to a SpikeSafe.
+
+    ...
+
+    Methods
+    -------
+    openSocket(self, ip_address, port_number)
+        Opens a TCP/IP socket for remote communication to a SpikeSafe
+    closeSocket(self)
+        Closes TCP/IP socket used for remote communication to a SpikeSafe
+    sendScpiCommand(self, scpi_command)
+        Sends a SCPI command via TCP/IP socket to a SpikeSafe
+    readData(self)
+        Reads data reply via TCP/IP socket from a SpikeSafe
+    """
 
     # create a connection via socket
     def openSocket(self, ip_address, port_number):
+        """Opens a TCP/IP socket for remote communication to a SpikeSafe
+
+        Parameters
+        ----------
+        ip_address : str
+            IP address of the SpikeSafe (10.0.0.220 to 10.0.0.0.251)
+        port_number : str
+            Port number of the SpikeSafe (8282 by default)
+
+        Raises
+        ------
+        Exception
+            On any error
+        """
         global tcp_socket
         try:
             # create socket with 2 second timeout and connect to SpikeSafe
