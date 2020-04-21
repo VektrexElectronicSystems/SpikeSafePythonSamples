25a8aec28767a69ec44dbaef0340ee6d6d374a48
biddyboyy <62398880+biddyboyy@users.noreply.github.com>
2020-04-21 16:35:08 -0700

    Updates Example Sequences
    Added content to all available sequences equivalent to their respective Development Tool templates

---
+++ RunModulatedMode.py
@@ -0,0 +1,69 @@
+# Goal: Connect to a SpikeSafe and run Modulated DC mode into a shorting plug with a custom pulse sequence
+# Expectation: Channel 1 will be driven with varying current levels up to 100mA, specified within SCPI commands
+
+import sys
+import time
+from Data.MemoryTableReadData import LogMemoryTableRead
+from Utility.SpikeSafeUtility.ReadAllEvents import LogAllEvents
+from Utility.SpikeSafeUtility.ReadAllEvents import ReadUntilEvent
+from Utility.SpikeSafeUtility.TcpSocket import TcpSocket
+from Utility.Threading import Wait     
+
+### set these before starting application
+
+# SpikeSafe IP address and port number
+ip_address = '10.0.0.241'
+port_number = 8282          
+
+### start of main program
+try:
+    # instantiate new TcpSocket to connect to SpikeSafe
+    tcp_socket = TcpSocket()
+    tcp_socket.openSocket(ip_address, port_number)
+
+    # reset to default state and check for all events,
+    # it is best practice to check for errors after sending each command      
+    tcp_socket.sendScpiCommand('*RST')                  
+    LogAllEvents(tcp_socket)
+
+    # set Channel 1's pulse mode to Modulated DC
+    tcp_socket.sendScpiCommand('SOUR1:FUNC:SHAP MODULATED')    
+
+    # set Channel 1's current to 100 mA
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.1')        
+
+    # set Channel 1's voltage to 40 V
+    tcp_socket.sendScpiCommand('SOUR1:VOLT 40') 
+
+    # set Channel 1's modulated sequence to run 50,000 pulses with 900ms off time and 100ms at the full set current
+    tcp_socket.sendScpiCommand('SOUR1:SEQ 50000(.9@0,.1@100)') 
+
+    # Log all events since all settings are sent
+    LogAllEvents(tcp_socket) 
+
+    # turn on Channel 1
+    tcp_socket.sendScpiCommand('OUTP1 1')                                         
+
+    # Wait until channel is ready for a trigger command
+    ReadUntilEvent(tcp_socket, 100) # event 100 is "Channel Ready"
+
+    # Output modulated sequence
+    tcp_socket.sendScpiCommand('OUTP1:TRIG')
+
+    # check for all events and measure readings on Channel 1 once per second for 30 seconds,
+    # it is best practice to do this to ensure Channel 1 is on and does not have any errors
+    time_end = time.time() + 30                         
+    while time.time() < time_end:                       
+        LogAllEvents(tcp_socket)
+        LogMemoryTableRead(tcp_socket)
+        Wait(1)                            
+    
+    # turn off Channel 1
+    tcp_socket.sendScpiCommand('OUTP1 0')               
+
+    # disconnect from SpikeSafe                      
+    tcp_socket.closeSocket()                            
+except Exception as err:
+    # print any error to terminal and exit application
+    print('Program error: {}'.format(err))          
+    sys.exit(1)
--- Utility/SpikeSafeUtility/ReadAllEvents.py
+++ Utility/SpikeSafeUtility/ReadAllEvents.py
@@ -59,6 +59,61 @@ def ReadAllEvents(spike_safe_socket):
         print("Error emptying event queue: {}".format(err))                                     
         raise
 
+def ReadUntilEvent(spike_safe_socket, code):
+    """Reads all SpikeSafe events from event queue
+
+    Parameters
+    ----------
+    spike_safe_socket : TcpSocket
+        Socket object used to communicate with SpikeSafe
+    code: int
+        Event code for desired event
+    
+    Returns
+    -------
+    EventData[]
+        All events from SpikeSafe leading to the desired event in a list of EventData objects
+
+    Raises
+    ------
+    Exception
+        On any error
+    """
+    try:
+        # event list to be returned to caller
+        event_data_list = []
+
+        # initialize flag to check if event queue is empty 
+        has_desired_event_occurred = False                                                                                                                      
+
+        # run as long as there is an event in the SpikeSafe queue
+        while has_desired_event_occurred == False:
+            # request SpikeSafe events and read data
+            spike_safe_socket.sendScpiCommand('SYST:ERR?')                                        
+            event_response = spike_safe_socket.readData()                                        
+
+            if event_response != '':
+                 # parse all valid event responses from SpikeSafe into event_data class                                                           
+                event_data = EventData().ParseEventData(event_response)
+
+                if event_data.code != code:
+                    # add all events prior to the desired event to the returned list
+                    event_data_list.append(event_data)                                  
+                else:
+                    event_data_list.append(event_data) 
+                    # Example message: 100, Channel Ready; Channel(s) 1
+                    has_desired_event_occurred = True                                                 
+            else:
+                # unexpected response detected from SpikeSafe, end checking
+                raise Exception('No event response from SpikeSafe: {}'.format(event_response))  
+
+        # return event list to caller        
+        return event_data_list                                                                  
+    except Exception as err:
+        # print any error to terminal and raise error to function caller
+        print("Error emptying event queue: {}".format(err))                                     
+        raise
+
 def LogAllEvents(spike_safe_socket):
     """Reads all SpikeSafe events from event queue and prints them to terminal
 
