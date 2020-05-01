a385aa45791a7cd29098dedb91b60aa407b47842
biddyboyy <62398880+biddyboyy@users.noreply.github.com>
2020-05-01 11:21:14 -0700

    Add README.md's, update DC Dynamic
    Improved folder hierarchy as well

--- Instrument Examples/Run DC/README.md
+++ Instrument Examples/Run DC/README.md
@@ -3,6 +3,8 @@
 ## Purpose
 How to use a SpikeSafe PRF or SMU to deliver high precision DC current to an LED or Laser.  These operation modes output constant current at the specified Set Current. Current is outputted with configurable current ramp and automated power reduction to protect devices under test.
 
+DC mode and DC Dynamic mode operate very similarly. Generally, Vektrex recommends running with the newer DC Dynamic mode in order to take advantage of dynamic current changes during operation, as well as more efficient internal calculations within the SpikeSafe.
+
 ## RunDcMode Overview
 Operates SpikeSafe as DC current source with single output current.
 
@@ -21,12 +23,11 @@ Operates SpikeSafe as DC current source with single output current.
 Operates SpikeSafe as DC current source with multiple output current. In DC Dynamic mode, the Set Current can be modified while the SpikeSafe is outputting current.  This example can be used to generate a software controlled stair case ramp. 
 
 ## RunDcDynamicMode Key Settings
-- **Steps:** 10mA, 20mA, 30mA, 40mA
-- **Set Current:** 100mA
+- **Set Current:** 50mA initially. While the channel is operating, the Set Current will be dynamically changed to 100mA, 150mA, 200mA, then back to 100mA.
 - **Compliance Voltage:** 10V
 - **Ramp Rate:** Default. Voltage will ramp as fast as 10V/sec. Current will ramp as fast as 1A/sec.
 
 ## RunDcDynamicMode Current Output
-- When running either sequence, one can expect to see the following current ramp. This image was acquired by measuring output current using a TCPA300 Current Probe into a MDO3024 Mixed Domain Oscilloscope
+- In addition to seeing the current ramp in the figure above, one can expect to see the following current modifications when running this DC Dynamic sequence. This image was acquired by measuring output current using a TCPA300 Current Probe into a MDO3024 Mixed Domain Oscilloscope
 
-![](DC_Ramp.png)
+![](DC_Dynamic_Current_Changes.png)
--- Instrument Examples/Run DC/RunDcDynamicMode.py
+++ Instrument Examples/Run DC/RunDcDynamicMode.py
@@ -1,5 +1,5 @@
-# Goal: Connect to a SpikeSafe and run DC Dynamic mode into a shorting plug for 60 seconds
-# Expectation: Channel 1 will be driven with 100mA with a forward voltage of ~100mV during this time
+# Goal: Connect to a SpikeSafe and run DC Dynamic mode into a shorting plug. Demonstrate the concept of changing current Set Point dynamically (while the channel is outputting)
+# Expectation: Channel 1 will initially be driven with 50mA with a forward voltage of < 1V during this time. While running change the current to 100mA, 150mA, 200mA, then 100mA again
 
 import sys
 import time
@@ -11,7 +11,7 @@ from spikesafe_python.utility.Threading import Wait
 ### set these before starting application
 
 # SpikeSafe IP address and port number
-ip_address = '10.0.0.246'
+ip_address = '10.0.0.220'
 port_number = 8282          
 
 ### start of main program
@@ -33,25 +33,49 @@ try:
     tcp_socket.sendScpiCommand('SOUR1:CURR:PROT 50')    
     LogAllEvents(tcp_socket) 
 
-    # set Channel 1's current to 100 mA and check for all events
-    tcp_socket.sendScpiCommand('SOUR1:CURR 0.1')        
+    # set Channel 1's current to 50 mA and check for all events
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.05')        
     LogAllEvents(tcp_socket)  
 
     # set Channel 1's voltage to 10 V and check for all events
-    tcp_socket.sendScpiCommand('SOUR1:VOLT 10')         
+    tcp_socket.sendScpiCommand('SOUR1:VOLT 20')         
     LogAllEvents(tcp_socket) 
 
     # turn on Channel 1 and check for all events
     tcp_socket.sendScpiCommand('OUTP1 1')               
     LogAllEvents(tcp_socket)                            
 
-    # check for all events and measure readings on Channel 1 once per second for 60 seconds,
+    # check for all events and measure readings on Channel 1 once per second for 5 seconds,
     # it is best practice to do this to ensure Channel 1 is on and does not have any errors
-    time_end = time.time() + 60                         
+    time_end = time.time() + 10                         
     while time.time() < time_end:                       
         LogAllEvents(tcp_socket)
         LogMemoryTableRead(tcp_socket)
-        Wait(1)                            
+        Wait(1)    
+
+    # While the channel is running, dynamically change the Set Current to 100mA. Check events and measure readings afterward
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.1')        
+    LogAllEvents(tcp_socket)
+    LogMemoryTableRead(tcp_socket)
+    Wait(1)
+
+    # While the channel is running, dynamically change the Set Current to 150mA. Check events and measure readings afterward
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.15')        
+    LogAllEvents(tcp_socket)
+    LogMemoryTableRead(tcp_socket)
+    Wait(1)
+
+    # While the channel is running, dynamically change the Set Current to 200mA. Check events and measure readings afterward
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.2')        
+    LogAllEvents(tcp_socket)
+    LogMemoryTableRead(tcp_socket)
+    Wait(1)
+
+    # While the channel is running, dynamically change the Set Current to 100mA. Check events and measure readings afterward
+    tcp_socket.sendScpiCommand('SOUR1:CURR 0.1')        
+    LogAllEvents(tcp_socket)
+    LogMemoryTableRead(tcp_socket)
+    Wait(1)
     
     # turn off Channel 1 and check for all events
     tcp_socket.sendScpiCommand('OUTP1 0')               
