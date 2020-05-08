439f98047dab61736a513884374198caa2ac105c
biddyboyy <62398880+biddyboyy@users.noreply.github.com>
2020-05-07 17:14:11 -0700

    Added Capture Full Pulse script
    sequence uses digitizer to measure voltage readings across a single pulse. Readings are plotted

--- instrument_examples/making_integrated_voltage_measurements/measure_voltage_across_pulse/MeasureVoltageAcrossPulse.py
+++ instrument_examples/making_integrated_voltage_measurements/measure_voltage_across_pulse/MeasureVoltageAcrossPulse.py
@@ -1,5 +1,6 @@
-# Goal: Connect to a SpikeSafe and run Pulsed mode into a shorting plug. Take voltage measurements from the pulsed output using the SpikeSafe SMU's integrated Digitizer
-# Expectation: Channel 1 will be driven with 100mA with a forward voltage of ~100mV during this time
+# Goal: Connect to a SpikeSafe and output a Single Pulse into a 10Ω resistor
+#       Take voltage measurements throughout that pulse using the SpikeSafe SMU's integrated Digitizer to determine the pulse shape
+# Expectation: Channel 1 will be driven with 100mA with a forward voltage of ~1V during this time
 
 import sys
 import time
@@ -8,6 +9,7 @@ from spikesafe_python.utility.spikesafe_utility.ReadAllEvents import log_all_eve
 from spikesafe_python.utility.spikesafe_utility.ReadAllEvents import read_until_event
 from spikesafe_python.utility.spikesafe_utility.TcpSocket import TcpSocket
 from spikesafe_python.utility.Threading import wait     
+from matplotlib import pyplot as plotter
 
 ### set these before starting application
 
@@ -30,9 +32,8 @@ try:
     tcp_socket.send_scpi_command('VOLT:ABOR')
 
     # set up Channel 1 for pulsed output. To find more explanation, see instrument_examples/run_pulsed
-    tcp_socket.send_scpi_command('SOUR1:FUNC:SHAP PULSED')
+    tcp_socket.send_scpi_command('SOUR1:FUNC:SHAP SINGLEPULSE')
     tcp_socket.send_scpi_command('SOUR1:PULS:TON 0.001')
-    tcp_socket.send_scpi_command('SOUR1:PULS:TOFF 0.009')
     tcp_socket.send_scpi_command('SOUR1:CURR:PROT 50')    
     tcp_socket.send_scpi_command('SOUR1:PULS:CCOM 4')
     tcp_socket.send_scpi_command('SOUR1:PULS:RCOM 4')
@@ -43,37 +44,40 @@ try:
     # set Digitizer voltage range to 10V since we expect to measure voltages significantly less than 10V
     tcp_socket.send_scpi_command('VOLT:RANG 10')
 
-    # set Digitizer aperture for 600µs. Aperture specifies the measurement time, and we want to measure a majority of the pulse's constant current output
-    tcp_socket.send_scpi_command('VOLT:APER 600')
+    # set Digitizer aperture for 2µs, the minimum value. Aperture specifies the measurement time, and we want to measure incrementally across the current pulse
+    tcp_socket.send_scpi_command('VOLT:APER 2')
 
-    # set Digitizer trigger delay to 200µs. We want to give sufficient delay to omit any overshoot the current pulse may have
-    tcp_socket.send_scpi_command('VOLT:TRIG:DEL 200')
+    # set Digitizer trigger delay to 0µs. We want to take measurements as fast as possible
+    tcp_socket.send_scpi_command('VOLT:TRIG:DEL 0')
 
-    # set Digitizer trigger source to hardware. When set to a hardware trigger delay, the digitizer waits for a trigger signal from the SpikeSafe to start a measurement
+    # set Digitizer trigger source to hardware. When set to a hardware trigger, the digitizer waits for a trigger signal from the SpikeSafe to start a measurement
     tcp_socket.send_scpi_command('VOLT:TRIG:SOUR HARDWARE')
 
     # set Digitizer trigger edge to rising. The Digitizer will start a measurement after the SpikeSafe's rising pulse edge occurs
     tcp_socket.send_scpi_command('VOLT:TRIG:EDGE RISING')
 
-    # set Digitizer trigger count to 525, the maximum value. 525 rising edges of current pulses will correspond to 525 voltage readings
-    tcp_socket.send_scpi_command('VOLT:TRIG:COUN 525')
+    # set Digitizer trigger count to 1. We are measuring the output of one current pulse
+    tcp_socket.send_scpi_command('VOLT:TRIG:COUN 1')
 
-    # set Digitizer reading count to 1. This is the amount of readings that will be taken when the Digitizer receives its specified trigger signal
-    tcp_socket.send_scpi_command('VOLT:READ:COUN 1')
+    # set Digitizer reading count to 525, the maximum value. We are measuring a 1ms pulse, and will take 525 measurements 2µs apart from each other
+    tcp_socket.send_scpi_command('VOLT:READ:COUN 525')
 
     # check all SpikeSafe event since all settings have been sent
     log_all_events(tcp_socket)
 
+    # initialize the digitizer. Measurements will be taken once a current pulse is outputted
+    tcp_socket.send_scpi_command('VOLT:INIT')
+
     # turn on Channel 1 
     tcp_socket.send_scpi_command('OUTP1 1')
 
-    # wait until Channel 1 is fully ramped before we take any digitizer measurements. We are looking to measure consistent voltage values
+    # wait until Channel 1 is ready to pulse
     read_until_event(tcp_socket, 100) # event 100 is "Channel Ready"
 
-    # start Digitizer measurements
-    tcp_socket.send_scpi_command('VOLT:INIT')
+    # output a current pulse for Channel 1
+    tcp_socket.send_scpi_command('OUTP1:TRIG')
 
-    # wait for the Digitizer measurements to complete
+    # wait for the Digitizer measurements to complete. This loop should only iterate once
     digitizerHasNewData = ''                       
     while digitizerHasNewData != b'TRUE\n':                       
         log_all_events(tcp_socket)
@@ -85,8 +89,21 @@ try:
 
     # fetch and print the Digitizer voltage readings
     tcp_socket.send_scpi_command('VOLT:FETC?')
-    digitizerData = tcp_socket.read_data_until_end()
-    print("Digitizer readings: {}".format(digitizerData))
+    digitizerData = tcp_socket.read_data()
+
+    # put the fetched data in a plottable data format
+    voltageReadingStrings = digitizerData.decode(sys.stdout.encoding).split(",")
+    voltageReadings = []
+    sampleNumbers = []
+    sample = 1
+    for v in voltageReadingStrings:
+        voltageReadings.append(float(v))
+        sampleNumbers.append(sample)
+        sample += 1
+
+    # plot the pulse shape using the voltage readings
+    plotter.plot(sampleNumbers, voltageReadings)
+    plotter.show()
 
     # turn off Channel 1 after routine is complete
     tcp_socket.send_scpi_command('OUTP1 0')
