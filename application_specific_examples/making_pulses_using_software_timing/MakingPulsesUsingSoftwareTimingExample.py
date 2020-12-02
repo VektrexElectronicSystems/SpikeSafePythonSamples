413a3aa2e013012aee3b3410676911f9aba1f882
eljay gemoto <eljay@vektrex.com>
2020-12-02 11:13:37 -0800

    Fixed default IP address

--- application_specific_examples/making_pulses_using_software_timing/MakingPulsesUsingSoftwareTimingExample.py
+++ application_specific_examples/making_pulses_using_software_timing/MakingPulsesUsingSoftwareTimingExample.py
@@ -12,7 +12,7 @@ from spikesafe_python.TcpSocket import TcpSocket
 ### set these before starting application
 
 # SpikeSafe IP address and port number
-ip_address = '10.0.0.246'
+ip_address = '10.0.0.220'
 port_number = 8282 
 
 ### setting up sequence log
