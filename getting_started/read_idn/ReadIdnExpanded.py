d1c6e3e9b50086364ade46681db9bb9080cb108a
eljay gemoto <eljay@vektrex.com>
2020-12-02 15:02:18 -0800

    Added Digitizer and FSSS to ReadIdnExpanded

--- getting_started/read_idn/ReadIdnExpanded.py
+++ getting_started/read_idn/ReadIdnExpanded.py
@@ -1,11 +1,24 @@
 # Goal: 
 # Connect to a SpikeSafe and request module identification
 # 
-# SCPI Command: 
+# SCPI Commands: 
 # *IDN?
+# VOLT:DIGI:AVAIL?
+# VOLT:VER?
+# VOLT:DATA:HWRE?
+# VOLT:DATA:SNUM?
+# VOLT:DATA:CDAT?
+# OUTP1:CONN:AVAIL?
 # 
 # Example Result: 
 # Vektrex, SpikeSafe Mini, Rev 2.0.3.18; Ch 1: DSP 2.0.9, CPLD C.2, Last Cal Date: 17 FEB 2020, SN: 12006, HwRev: E1, Model: MINI-PRF-10-10US\n
+# TRUE
+# 0.9.0
+# C
+# 50012
+# 02 DEC 2020
+# ALL
+
 
 import sys
 import logging
@@ -38,6 +51,32 @@ try:
     # read SpikeSafe information
     data = tcp_socket.read_data()                    
     log.info("SpikeSafe *IDN? Response: {}".format(data))
+
+    # request if Digitizer is available (This is only available on PSMU and PSMU HC depending on model)
+    tcp_socket.send_scpi_command('VOLT:DIGI:AVAIL?')         
+
+    # read Digitizer information
+    data = tcp_socket.read_data()
+    log.info("SpikeSafe VOLT:DIGI:AVAIL? Response: {}".format(data))
+    if data == "TRUE":        
+        tcp_socket.send_scpi_command('VOLT:VER?')             
+        digitizer_version = tcp_socket.read_data()
+        tcp_socket.send_scpi_command('VOLT:DATA:HWRE?')             
+        digitizer_hardware_rev = tcp_socket.read_data()    
+        tcp_socket.send_scpi_command('VOLT:DATA:SNUM?')             
+        digitizer_serial_number = tcp_socket.read_data()        
+        tcp_socket.send_scpi_command('VOLT:DATA:CDAT?')             
+        digitizer_calibration_date = tcp_socket.read_data()                        
+
+        log.info("Digitizer Information Response: version={}, HW Rev={}, SN={}, Cal Date={}".format(digitizer_version, digitizer_hardware_rev, digitizer_serial_number, digitizer_calibration_date))
+
+    # request if Force Sense Selector Switch is available (This is only available on PSMU and PSMU HC depending on model)
+    tcp_socket.send_scpi_command('OUTP1:CONN:AVAIL?')
+
+    # read Force Sense Selector Switch information
+    data = tcp_socket.read_data()
+
+    log.info("SpikeSafe OUTP1:CONN:AVAIL? Response: {}".format(data))
 
     # disconnect from SpikeSafe
     tcp_socket.close_socket()  
